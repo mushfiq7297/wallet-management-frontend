9cd5ab8c285b245799f5865544146a20797bc4a4
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-10-18 17:12:10 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+   <>
+   <div>
+    <h1>This is SecurePay Wallet management system</h1>
+    <Button>Click me</Button>
+   </div>
+   </>
+  )
+}
+
+export default App
