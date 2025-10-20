2d49a804dde554afc1408142a4257ee121854fbd
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-10-20 14:03:38 +0600

    add login and registerForm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import App from "@/App";
 
 import About from "@/pages/About";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 
 import { createBrowserRouter } from "react-router";
 
@@ -15,5 +17,13 @@ export const router = createBrowserRouter([
             }
         ]
     },
+    {
+        Component: Login,
+        path: "/login"
+    },
+    {
+        Component: Register,
+        path: "/register"
+    }
     
 ])
